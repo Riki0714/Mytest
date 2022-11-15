294d5e8177849432fd63f89db67bf0322152f6ee
Riki0714 <2036771078@qq.com>
2022-11-15 23:03:44 +0800

    just test

---
+++ wa.c
@@ -0,0 +1,22 @@
+/*********************************************************************************
+ *      Copyright:  (C) 2022 tbd...
+ *                  All rights reserved.
+ *
+ *       Filename:  new.c
+ *    Description:  This file 
+ *                 
+ *        Version:  1.0.0(2022年11月15日)
+ *         Author:  Riki_xiao <2036771078@qq.com>
+ *      ChangeLog:  1, Release initial version on "2022年11月15日 23时01分54秒"
+ *                 
+ ********************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+	printf("new!\n");
+	return 0;
+}
+
+
